27aa79d75b2420dba713a08a076b8fd04bc2c02f
slodkaoliwka <otunikowska@gmail.com>
2022-07-16 09:31:47 +0200

    Added second container, text to lists of recipes, fixes contact

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const entryPath = ".";
+
+module.exports = {
+    entry: `/js/app.js`,
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, `${entryPath}/build`)
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `${entryPath}`),
+        publicPath: "/build/",
+        compress: true,
+        port: 3001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
