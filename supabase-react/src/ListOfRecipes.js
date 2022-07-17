061f82ab9a2b201f03d285962c4ed4d78cbf5f91
slodkaoliwka <otunikowska@gmail.com>
2022-07-17 15:00:58 +0200

    Added API, new main site

---
+++ supabase-react/src/ListOfRecipes.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { supabase } from './supabaseClient'
+
+// lista przepisów
+<div class="card">
+  <img src="link do zdjêcia" class="card-img-top" alt="nazwa przepisu"/>
+  <div class="card-body">
+    <h5 class="card-title">Nazwa przepisu</h5>
+    {/*<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>*/}
+    <a href="#!" class="btn btn-primary">Wypróbuj!</a>
+  </div>
+</div>
