c7c4e26ac6d157c17b1a4ecda693cf77901028b6
slodkaoliwka <otunikowska@gmail.com>
2022-07-25 19:50:52 +0200

    Added gallery.js

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+//galeria
+
+var testi = $('#testimonial-slide');
+var owlcarouselslide = function (e) {
+
+    // testimonial slide
+    testi.owlCarousel({
+        autoplay: false,
+        center: true,
+        loop: true,
+        dots: true,
+        touchDrag: false,
+        mouseDrag: false,
+        smartSpeed: 900,
+        items: 1,
+        margin: 100,
+        nav: true,
+        navText: [
+            '<i class="fa fa-long-arrow-left " aria-hidden="true"></i>',
+            '<i class="fa fa-long-arrow-right " aria-hidden="true"></i>'
+        ],
+        navContainer: '.custom-nav',
+    });
+};
+
+$(document).ready(function () {
+    owlcarouselslide();
+}); 
